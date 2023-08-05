d45b2f332a298024ab839b8913479b891d2564d0
Oon Weng Wai <ElricOon@users.noreply.huggingface.co>
2023-08-05 07:13:51 +0000

    Update description, steps, file uploader
    Update description, steps, file uploader

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ def main():
         st.markdown("## Description")
         st.markdown("""
         - This is an eye disease classification system that allows users to upload retinal images and perform predictions.
-        - The system can predict the following classes: Cataracts, Diabetic Retinopathy, Glaucoma, and Normal.
-        """)
+        - The system can predict the following classes: <p>
+                    <span style="color: Red;"><strong>Cataract</strong></span>, 
+                    <span style="color: Red;"><strong>Diabetic Retinopathy</strong></span>, 
+                    <span style="color: Red;"><strong>Glaucoma</strong></span>, 
+                    <span style="color: Red;"><strong>Normal</strong></span>.</p>
+        """, unsafe_allow_html=True)
 
         st.markdown("## Steps")
         st.markdown("""
-        1. Click the "Upload Image" button to upload a retinal image.
+        1. Click the "Browse files" button to upload a retinal image.
         2. Click the "Predict Eye Disease" button to start the eye disease prediction.
         3. Please wait for a few moments to get the result.
         4. The result will be displayed below the image.
@@ -73,7 +77,7 @@ def load_model():
 
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Pick an image to test')
+    uploaded_file = st.file_uploader(label='Pick an retinal image to test')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
